2b1ff54da999b4051c1253483bb7ba71729efe0b
viviane santos <vjsantos.viviane@gmail.com>
2025-05-21 10:13:54 -0300

    fear: add function ft_print_alphabet.c

---
+++ C/C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+//function that display the alphabet in lowercase
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
